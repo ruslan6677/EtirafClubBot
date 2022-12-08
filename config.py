9a620b3f9d8a4e71c6c5b8d3486bbf3ff0342753
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2022-12-08 13:58:10 +0400

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ qrupstart = os.environ.get("qrupstart")
 gonderildi = os.environ.get("gonderildi")
 support = os.environ.get("support")
 sahib = os.environ.get("sahib")
-Support 2 = os.environ.get("support2")
+support 2 = os.environ.get("support2")
 #
 # RoBotlarimTg
 # RoBotlarimTg
